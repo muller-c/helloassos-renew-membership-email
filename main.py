6893ee48fcaa3e604d2bcd69cfd764f0d9a05f81
muller-c <50455496+muller-c@users.noreply.github.com>
2020-06-14 22:47:05 +0200

    Update main.py
    add template from db

--- main.py
+++ main.py
@@ -115,17 +115,7 @@ def send_email(association, adherant):
 
     # Create the body of the message (a plain-text and an HTML version).
     text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
-    html = """\
-    <html>
-      <head></head>
-      <body>
-        <p>Hi!<br>
-           How are you?<br>
-           Here is the <a href="http://www.python.org">link</a> you wanted.
-        </p>
-      </body>
-    </html>
-    """
+    html = association.email_template
 
     # Record the MIME types of both parts - text/plain and text/html.
     part1 = MIMEText(text, 'plain')
